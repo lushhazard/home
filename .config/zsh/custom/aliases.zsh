b269b53f755b936ada01d2e884f139bc89b8d03e
lushhazard <your@mom.gay>
2025-09-21 19:45:11 +0200

    meow

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -11,6 +11,6 @@ alias wine="LANG=EN wine"
 alias h='hyprland'
 
 #eza
-alias e="eza -l --time-style '+%Y年%m月%d日 %H:%M' --icons=always"
-alias eg="eza -l --git --git-repos --time-style '+%Y年%m月%d日 %H:%M' --icons=always"
+alias e="eza -l --time-style '+%Y年%m月%d日 %H:%M'"
+alias eg="eza -l --git --git-repos --time-style '+%Y年%m月%d日 %H:%M'"
 alias ez='eza'
